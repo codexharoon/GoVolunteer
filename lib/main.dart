059474c3e83aa985f0d5817ad3f568d71259d726
Syed Muhammad Rafay Hassni <100311424+rafaysyed1@users.noreply.github.com>
2024-05-18 07:57:59 +0500

    Update the folder structure from seperate folders to keep screens in the main screen folder for better readablity

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:go_volunteer/splashscreen/splash_screen.dart';
+import 'package:go_volunteer/screens/splash_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
